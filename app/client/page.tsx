dd323c5ba086259aab0630bf0d1092188cdda40f
PetxCode <brighterdayscodelab@gmail.com>
2024-07-15 15:19:15 +0100

    adding client screen

---
+++ app/client/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div>This is the Client</div>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/home/page.tsx
@@ -0,0 +1,79 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div className="carousel rounded-box">
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+            alt="Burger"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+            alt="Burger"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+            alt="Burger"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
+            alt="Burger"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+            alt="Burger"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
+            alt="Burger"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+            alt="Burger"
+          />
+        </div>
+      </div>
+
+      <div>
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <div>
+        <button className="btn btn-warning">Neutral</button>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <div>
+        <Button variant="destructive">Button</Button>
+      </div>
+    </div>
+  );
+};
+
+export default page;
